fcaef39560b81b3a29524f23710eb1cb354a1ef0
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-09-21 22:44:32 -0300

    Inclusão do HTML,Javascript.

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros:5,
+        candidatos:[
+            {
+                numero:19564,
+                nome:'Mercedes de Souza',
+                partido:'EDD',
+                fotos:[
+                    {url:'19564.jpg',legenda:'Vereador',small:false}
+                ]
+            },
+            {
+                numero:20152,
+                nome:'Vitor Ramos de Souza Alexandre',
+                partido:'EDD',
+                fotos:[
+                    {url:'20152.jpg',legenda:'Vereador',small:false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros:2,
+        candidatos:[
+            {
+                numero:00,
+                nome:'Beatriz Ramos de Souza Alexandre',
+                partido:'GERAÇÃO1',
+                vice:'Thayná de Souza Alexandre Barrera',
+                fotos:[
+                    {url:'00.jpg',legenda:'Prefeito'},
+                    {url:'00_2',legenda:'Vice-Prefeito',small:true}
+                ]
+            },
+            {
+                numero:18,
+                nome:'Gabriel de Souza Alexandre Scachette',
+                partido:'GERAÇÃO4',
+                vice:'Laura Ramos de Souza Alexandre',
+                fotos:[
+                    {url:'18.jpg',legenda:'Prefeito'},
+                    {url:'18_2',legenda:'Vice-Prefeito',small:true}
+                ]
+            },
+        ]
+    }
+]
